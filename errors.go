bdac6f446a715744642d1d19f2925799a7b9eaa8
Vishal T <vtalwar.w@gmail.com>
2023-01-17 15:15:23 -0800

    initial commit

---
+++ errors.go
@@ -0,0 +1,38 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"runtime/debug"
+)
+
+type AppError struct {
+	Inner      error
+	Message    string
+	StackTrace string
+}
+
+func NewAppError(errMsg string, msgArgs ...interface{}) AppError {
+	errMsg = fmt.Sprintf(errMsg, msgArgs...)
+	return AppError{
+		Inner:      errors.New(errMsg),
+		Message:    errMsg,
+		StackTrace: string(debug.Stack()),
+	}
+}
+
+func WrapError(err error, msgf string, msgArgs ...interface{}) AppError {
+	errMsg := err.Error()
+	if errMsg != "" {
+		errMsg = fmt.Sprintf(msgf, msgArgs...)
+	}
+	return AppError{
+		Inner:      err,
+		Message:    errMsg,
+		StackTrace: string(debug.Stack()),
+	}
+}
+
+func (err AppError) Error() string {
+	return err.Message
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/comfforts/errors
+
+go 1.19
